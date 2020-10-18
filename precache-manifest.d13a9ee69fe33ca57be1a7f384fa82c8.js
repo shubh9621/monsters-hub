4c300bae870d2d3f1370dd7e3f8fe8f78cfaffe1
shubh9621 <shubh_9621@outlook.com>
2020-10-18 15:27:39 +0530

    Updates

---
+++ precache-manifest.d13a9ee69fe33ca57be1a7f384fa82c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e48a582f44701af702654ed2202e4d1",
+    "url": "/monsters-hub/index.html"
+  },
+  {
+    "revision": "186f2abf244b3b0e69c8",
+    "url": "/monsters-hub/static/css/main.68dcf8be.chunk.css"
+  },
+  {
+    "revision": "908a2a11cc6fcb12e9d2",
+    "url": "/monsters-hub/static/js/2.100bcac7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-hub/static/js/2.100bcac7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "186f2abf244b3b0e69c8",
+    "url": "/monsters-hub/static/js/main.07f2a38d.chunk.js"
+  },
+  {
+    "revision": "af0383e2923bde3ffb66",
+    "url": "/monsters-hub/static/js/runtime-main.bf4d3222.js"
+  }
+]);
